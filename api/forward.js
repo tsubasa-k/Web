87afddb12815f94c44e493d26c21d179abe681a9
張子姍 <61736148+tsubasa-k@users.noreply.github.com>
2025-07-26 22:32:38 +0800

    Update forward.js

--- api/forward.js
+++ api/forward.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const ref = req.headers['referer'] || "None";
 
   // 換成你的 Google Apps Script Web App URL（/exec 結尾）
-  const gscriptURL = "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec";
+  const gscriptURL = "https://script.google.com/macros/s/AKfycbyM9jZAnb1q-4lpv8xXZcJzARjWIzbtC-qr7uYxPI0EiL09hkZdmNCVUbnaST4NECh0/exec";
   const params = new URLSearchParams({
     ip: ip,
     userAgent: ua,
