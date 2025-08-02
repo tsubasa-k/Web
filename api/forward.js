4e4896b729cd79d785a53d0d448037baae4c0487
張子姍 <61736148+tsubasa-k@users.noreply.github.com>
2025-08-02 20:00:37 +0800

    Update forward.js

--- api/forward.js
+++ api/forward.js
@@ -1,14 +1,27 @@
 export default async function handler(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || "unknown";
-  const ua = req.headers['user-agent'] || "unknown";
-  const ref = req.headers['referer'] || "None";
 
-  // 替換成你的 Google Apps Script Web App URL（/exec 結尾）
-  const gscriptURL = "https://script.google.com/macros/s/AKfycbyyv_X5fERNpIs53cVbR6Vsqoo2wPmQuws3SIPBRwHITRjDO5CSwxgHIujlnFMxI2Ug/exec";
+  const {
+    userAgent = "unknown",
+    referrer = "None",
+    resolution = "unknown",
+    platform = "unknown",
+    language = "unknown",
+    timezone = "unknown",
+    cores = "unknown"
+  } = req.body ? JSON.parse(req.body) : {};
+
+  const gscriptURL = "https://script.google.com/macros/s/AKfycbxMbACS4gM7-k67MClyNOvpnjQWgDhENZpEZKwgzxvBdg4WegdPAldbhL5A2vzZz4_X/exec"; // 替換為你的 Apps Script URL
+
   const params = new URLSearchParams({
-    ip: ip,
-    userAgent: ua,
-    referrer: ref
+    ip,
+    userAgent,
+    referrer,
+    resolution,
+    platform,
+    language,
+    timezone,
+    cores
   });
 
   await fetch(`${gscriptURL}?ts=${Date.now()}`, {
