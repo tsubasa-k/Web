0a35cefbdcfa3ff8db7d00a7f9722c3b3a37949e
張子姍 <61736148+tsubasa-k@users.noreply.github.com>
2025-08-04 23:57:29 +0800

    Update forward.js

--- api/forward.js
+++ api/forward.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const log = `[${new Date().toISOString()}] OS: ${os} | MAC: ${mac} | UUID: ${uuid}`;
   console.log("Received:", log);
 
-  await fetch("https://script.google.com/macros/s/AKfycbynoB555JuBUhbEbeJYr8FafVSDcssL1qigSbiJTLUkD_YbBOy_-jnMdLgR8sG0eXWA/exec", {
+  await fetch("https://script.google.com/macros/s/AKfycbzYlrtp8nu7_q86ScZz-YrmJ-ptx3WZQV1hQWhwMa1jfzHqmMUwBW9_yPO8zHA6lXxg/exec", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams({ mac, uuid, os })
